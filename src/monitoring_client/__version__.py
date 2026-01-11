cd47fe2c02c323ecc56bb6b6e0ecc68dccd426f4
Frédéric GIL GARCIA <frederic.gilgarcia@gmail.com>
2026-01-11 17:37:35 +0100

    chore: bump version to 1.0.51

--- src/monitoring_client/__version__.py
+++ src/monitoring_client/__version__.py
@@ -1,7 +1,7 @@
 """Version information for monitoring-client."""
 
-__version__ = "1.0.50"
-__version_info__ = (1,0,50)  # Version sous forme de tuple (major, minor, patch)
+__version__ = "1.0.51"
+__version_info__ = (1,0,51)  # Version sous forme de tuple (major, minor, patch)
 __author__ = "Frederic GIL GARCIA"
 __license__ = "MIT"
 __email__ = "frederic.gilgarcia@gmail.com"
